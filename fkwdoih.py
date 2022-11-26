09030bd3389da6b2c7ce9a3807952abfba3de5a9
oxxrayy <114632258+oxxrayy@users.noreply.github.com>
2022-11-26 14:16:09 +0700

    Update fkwdoih.py

--- fkwdoih.py
+++ fkwdoih.py
@@ -1,11 +1,23 @@
 from PIL import Image, ImageDraw, ImageFont
+def f(t, i):
+    im = Image.new('RGB', (800,600), color=('#FAACAC'))
+    font = ImageFont.truetype('C:/Users/user/Desktop/arial.ttf', size=22)
+    draw_text = ImageDraw.Draw(im)
+    draw_text.text((100,100),
+    t,
+    font = font,
+    fill=('#7d69df'))
+    im.save()
 
-im = Image.new('RGB', (800,600), color=('#FAACAC'))
-font = ImageFont.truetype('C:/Users/user/Desktop/arial.ttf', size=18)
-draw_text = ImageDraw.Draw(im)
-draw_text.text((100,100),
-    'опрл',
-    font=font,
-    fill=('#1C0606'))        
-im.show()
-            
+t=['В ад перед тобой, в рай после тебя',
+'Вместе с тобой, что можно',
+'Вместо тебя всё то, что нельзя',
+'В рай только после тебя',
+'В ад только перед тобой',
+'В ад перед тобой, в рай после тебя',
+'Вместе с тобой, что можно',
+'Вместо тебя всё то, что нельзя',
+'В рай только после тебя',
+'В ад только перед тобой' ]
+for i in range (10):
+    f (t[i], i)
